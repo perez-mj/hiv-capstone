3271dcfbcf5f0cf6d7600ba96843e5c4616e222f
MJ Perez <MJPerez>
2026-09-17 10:30:58 +0800

    added multichain docker compose

--- backend/config/blockchain.js
+++ backend/config/blockchain.js
@@ -5,7 +5,7 @@ module.exports = {
   port:     process.env.BLOCKCHAIN_PORT || '8571',
   rpcPort:  process.env.BLOCKCHAIN_RPC_PORT || '8572',
   chain:    process.env.BLOCKCHAIN_CHAIN || 'omph_hiv_chain',
-  stream:   process.env.BLOCKCHAIN_STREAM || 'patient_audit',   // <-- ADD THIS
-  user:     process.env.BLOCKCHAIN_USER || 'multichainrpc',
-  password: process.env.BLOCKCHAIN_PASSWORD || '',
+  stream:   process.env.BLOCKCHAIN_STREAM || 'patient_audit',   
+  user:     process.env.BLOCKCHAIN_RPC_USER || 'multichainrpc',
+  password: process.env.BLOCKCHAIN_RPC_PASSWORD || '',
 };
